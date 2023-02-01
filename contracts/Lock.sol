2124a34ab9cee870cd0f5de7a09dad0ad8d5f5bd
Prakhar Sharma <prakharentp2002@gmail.com>
2023-02-01 18:50:00 +0530

    Done

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,27 +4,27 @@ pragma solidity ^0.8.0;
 contract WeatherFeed {
 
     struct WeatherData {
-        uint256 temperature;
-        uint256 humidity;
-        uint256 windSpeed;
+        string temperature;
+        string description;
     }
 
     event weather(string _location);
 
     mapping (string => WeatherData) public weatherData;
 
-    function updateWeather(string memory _location, uint256 _temperature, uint256 _humidity, uint256 _windSpeed) public {
+    function updateWeather(string memory _location, string memory _temperature, string memory _description) public {
         weatherData[_location].temperature = _temperature;
-        weatherData[_location].humidity = _humidity;
-        weatherData[_location].windSpeed = _windSpeed;
+        weatherData[_location].description = _description;
     }
 
-    function getWeather(string memory _location) public returns(uint256, uint256, uint256) {
- 
+    function requestWeather(string memory _location) public returns(string memory) {
         emit weather(_location);
+        return "Successfully requested Weather details for Your location!";
+    }
+
+    function getWeather(string memory _location) public view returns(string memory, string memory){
         WeatherData memory currentWeather = weatherData[_location];
-        return (currentWeather.temperature, currentWeather.humidity, currentWeather.windSpeed);
-        
+        return (currentWeather.temperature, currentWeather.description);
     }
 }
 
--- scripts/index.js
+++ scripts/index.js
@@ -3,136 +3,146 @@ const axios = require('axios');
 
 async function getWeatherData(){
 
-    const ContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    const ContractAddress = "0x998abeb3E57409262aE5b751f60747921B33613E"
 
     const abi = [
         {
-        "anonymous": false,
-        "inputs": [
+          "anonymous": false,
+          "inputs": [
             {
-            "indexed": false,
-            "internalType": "string",
-            "name": "_location",
-            "type": "string"
+              "indexed": false,
+              "internalType": "string",
+              "name": "_location",
+              "type": "string"
             }
-        ],
-        "name": "weather",
-        "type": "event"
+          ],
+          "name": "weather",
+          "type": "event"
         },
         {
-        "inputs": [
+          "inputs": [
             {
-            "internalType": "string",
-            "name": "_location",
-            "type": "string"
+              "internalType": "string",
+              "name": "_location",
+              "type": "string"
             }
-        ],
-        "name": "getWeather",
-        "outputs": [
+          ],
+          "name": "getWeather",
+          "outputs": [
             {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
+              "internalType": "string",
+              "name": "",
+              "type": "string"
             },
             {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-            },
-            {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
+              "internalType": "string",
+              "name": "",
+              "type": "string"
             }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "function"
+          ],
+          "stateMutability": "view",
+          "type": "function"
         },
         {
-        "inputs": [
+          "inputs": [
             {
-            "internalType": "string",
-            "name": "_location",
-            "type": "string"
-            },
-            {
-            "internalType": "uint256",
-            "name": "_temperature",
-            "type": "uint256"
-            },
-            {
-            "internalType": "uint256",
-            "name": "_humidity",
-            "type": "uint256"
-            },
-            {
-            "internalType": "uint256",
-            "name": "_windSpeed",
-            "type": "uint256"
+              "internalType": "string",
+              "name": "_location",
+              "type": "string"
             }
-        ],
-        "name": "updateWeather",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
+          ],
+          "name": "requestWeather",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "nonpayable",
+          "type": "function"
         },
         {
-        "inputs": [
+          "inputs": [
             {
-            "internalType": "string",
-            "name": "",
-            "type": "string"
-            }
-        ],
-        "name": "weatherData",
-        "outputs": [
-            {
-            "internalType": "uint256",
-            "name": "temperature",
-            "type": "uint256"
+              "internalType": "string",
+              "name": "_location",
+              "type": "string"
             },
             {
-            "internalType": "uint256",
-            "name": "humidity",
-            "type": "uint256"
+              "internalType": "string",
+              "name": "_temperature",
+              "type": "string"
             },
             {
-            "internalType": "uint256",
-            "name": "windSpeed",
-            "type": "uint256"
+              "internalType": "string",
+              "name": "_description",
+              "type": "string"
             }
-        ],
-        "stateMutability": "view",
-        "type": "function"
+          ],
+          "name": "updateWeather",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "name": "weatherData",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "temperature",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "description",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
         }
-    ]
+      ]
 
     const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545/');
     const signer = provider.getSigner()
     const contract = new Contract(ContractAddress, abi, signer)
 
-    const getData = await contract.getWeather("delhi")
+    const getData = await contract.requestWeather("mumbai")
     const getDataRecipt = await getData.wait()
 
     const location = getDataRecipt.events[0].args._location;
-    console.log(location)
+    // console.log(location)
 
-    console.log(getWeatherDataOffChain(location))
+    var [temp, des ] = await getWeatherDataOffChain(location)
+    temp = temp.toString()
+    // console.log(temp, des)
+    const updateData = await contract.updateWeather(location, temp, des)
+    const finalData = await contract.getWeather(location)
+    console.log(finalData)
+
 
 }
 
-async function getWeatherDataOffChain(locations){
-    const params = {
-        access_key: 'bbca4a5fddad688e0cfd6b59612e8c6c',
-        query: 'New York'
+async function getWeatherDataOffChain(location){
+    try {
+        const apiKey = 'c6b644a9f69f2b39e4bbf0178303d637';
+        const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+        const response = await axios.get(weatherAPI);
+        const weatherData = response.data;
+        // console.log(`Current weather in ${location}:`);
+        // console.log(`Temperature: ${weatherData.main.temp}°F`);
+        // console.log(`Description: ${weatherData.weather[0].description}`);
+        return [weatherData.main.temp, weatherData.weather[0].description]
+      } catch (error) {
+        console.error(error);
       }
-      
-      axios.get('https://api.weatherstack.com/current', {params})
-        .then(response => {
-          const apiResponse = response.data;
-          console.log(apiResponse);
-        }).catch(error => {
-          console.log(error);
-        });
 }
 getWeatherData()
