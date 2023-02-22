cdd6ce15e89955e2d885fadc6727d9ae339e1002
PrakharSharma888 <prakharentp2002@gmail.com>
2023-02-22 17:47:09 +0530

    Done

--- scripts/index.js
+++ scripts/index.js
@@ -12,8 +12,6 @@ const privateKey =
 const wallet = new ethers.Wallet(privateKey, provider);
 const contract = new ethers.Contract(contractAddress, abi, wallet);
 
-done
-
 const oracleFunction = async () => {
  contract.on('weather', async (_loaction) => {
     console.log('--details--', _loaction);
